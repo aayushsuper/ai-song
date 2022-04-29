f60759d143a8b5bb5de6b136155bbac4aefbbd7d
aayushsuper <90053725+aayushsuper@users.noreply.github.com>
2022-04-29 16:43:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+Peter_pan_song="";
+fearless_song="";
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
+scoreleftWrist = 0;
+scorerightWrist = 0;
+song_fight_back = "";
+song_fearless = "";
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
+
+function preload(){
+    fight_back_song = loadSound("fight back.mp3");
+    Harry_potter_theme_song = loadSound("fearless.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+
+    fill("#00ff00");
+    stroke("#ff0000");
+
+    song_fight_back = fight_back_song.isPlaying();
+    console.log(song_fight_back);
+
+   
+    console.log(song_fearless);
+
+    if(scoreleftWrist > 0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+        fearless_song.stop();
+        if(song_fight_back == false){
+            fight_back_song.play();
+        }
+        else{
+            console.log("Song Name: fight Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Fight Back Song";
+        }
+    }
+
+    if(scorerightWrist > 0.2){
+        circle(rightWrist_x,rightWrist_y,20);
+        fight_back_song.stop();
+        if(song_fearless == false){
+            fearless_song.play();
+        }
+        else{
+            console.log("Song Name: Fearless Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Fearless";
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log(scoreleftWrist);
+
+        scorerightWrist = results[0].pose.keypoints[10].score;
+        console.log(scorerightWrist);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
+}
